f72c39a87fe07fbe54191ab48892b93ed7e5e1ab
root <6313504@qq.com>
2018-09-06 21:13:16 +0800

    ImageUtil

--- LibTool/ImageUtil.cs
+++ LibTool/ImageUtil.cs
@@ -4,25 +4,25 @@ using System.IO;
 
 namespace DaiChong.Lib.Util
 {
-    //public class ImageUtil
-    //{
-    //    public static string ImageToBase64Str(Image Img, System.Drawing.Imaging.ImageFormat imageformat)
-    //    {
-    //        System.IO.MemoryStream MStream = new System.IO.MemoryStream();
-    //        Img.Save(MStream, imageformat);
-    //        byte[] ImgBuffer = MStream.GetBuffer();
-    //        return Convert.ToBase64String(ImgBuffer);
-    //    }
+    public class ImageUtil
+    {
+        public static string ImageToBase64Str(Image Img, System.Drawing.Imaging.ImageFormat imageformat)
+        {
+            System.IO.MemoryStream MStream = new System.IO.MemoryStream();
+            Img.Save(MStream, imageformat);
+            byte[] ImgBuffer = MStream.GetBuffer();
+            return Convert.ToBase64String(ImgBuffer);
+        }
 
-    //    public static byte[] ImageToBytes(Image img, System.Drawing.Imaging.ImageFormat imageformat)
-    //    {
-    //        MemoryStream ms = new MemoryStream();
-    //        img.Save(ms, imageformat);
-    //        byte[] by = new byte[ms.Length];
-    //        ms.Position = 0;
-    //        ms.Read(by, 0, Convert.ToInt32(ms.Length));
-    //        ms.Close();
-    //        return by;
-    //    }
-    //}
+        public static byte[] ImageToBytes(Image img, System.Drawing.Imaging.ImageFormat imageformat)
+        {
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, imageformat);
+            byte[] by = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(by, 0, Convert.ToInt32(ms.Length));
+            ms.Close();
+            return by;
+        }
+    }
 }
